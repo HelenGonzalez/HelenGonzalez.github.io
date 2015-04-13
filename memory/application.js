8856a3d7f277c22850f6a23fa0e0627aa0584413
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-04-13 13:39:00 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"];
 
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
-var card1="none"
-//making the cards appear=struggles
-$('.card').on('click',function()){
-  $mycard=(this). find('p');
-  $mycard.css('opacity',1);
-  card1=$mycard.html();
+  var card1="none";
+  //making the cards appear=struggles
+  $('.card').on('click',function()){
+    $mycard=(this).find('p');
+    $mycard.css('opacity',1);
+    card1=$mycard.html();
+  });
 });
