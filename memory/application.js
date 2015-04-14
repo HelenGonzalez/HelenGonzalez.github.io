98ebfbd8bf410006b9c01474d21022da7d44e674
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-04-14 13:12:34 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
   var card1="none";
   $('.card').on('click',function(){
     $(this).find('p').css('opacity',1);
-    card1= $('none'.html());
+    card1= $('card1'.html());
   });
 });
