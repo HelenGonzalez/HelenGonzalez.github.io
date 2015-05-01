a00944a896a49cc473733ffe7cd48e66f9d188f1
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-05-01 13:40:09 -0400

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,1 +1,8 @@
+from random import randint
 
+dice_outcome = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
+roll_store =[]
+
+for key in range(500):
+  save = randint(1,6) + randint(1,6)
+  
