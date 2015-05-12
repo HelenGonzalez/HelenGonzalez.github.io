1a31b95bff6502b4c0f5ce9c25bfc8d003262b3c
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-05-12 13:40:06 -0400

    Update election.py

--- election.py
+++ election.py
@@ -3,12 +3,12 @@ win=0
 for i in range(10000):
   regions = 0
   if random(1,100) <= 87:
-    regions +=1
+    regions +=1 #adds to region
     if randint(1,100) <= 65:
       regions += 1
     if randint(100) <= 17:
       regions += 1
   if region >= 2:
-    win += 1
+    win += 1 #adds to wins 
 
 print "Candidate A would have won the elections %s percent." %(win/range(10000))
