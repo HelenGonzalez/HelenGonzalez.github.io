eea1b6dc6db214428efa3af617e11b2903323831
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-05-12 13:15:15 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,1 +1,7 @@
 from random import random
+
+for i in range(10000):
+  if random.random(100) <= 87, and random.random(100) <= 65 or random.random(100)  <= 87, and random.random(100) <= 17, or random.ramdom(100) <= 65, or rsndom.random(100) <= 17:
+    print "Canditdate A wins"
+  else:
+    print "Candidate B wins"
