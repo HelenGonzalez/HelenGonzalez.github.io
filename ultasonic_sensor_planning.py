9658db511ba91b219f1b73d16ec97e5e7dc5e78e
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-05-27 13:48:47 -0400

    Update ultasonic_sensor_planning.py

--- ultasonic_sensor_planning.py
+++ ultasonic_sensor_planning.py
@@ -1,6 +1,13 @@
-gopigo import *
-if us_dist(15) > 10;
-  fwd()
-else:
-  turn_right()
+from gopigo import *
+def turn_right():
+  enc_tgt(1,0,14)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+  
+while (1 > 0)
+  if us_dist(15) > 10;
+    fwd()
+  else:
+    turn_right()
   
