536ef782746876ca0cae4e016c547f3e96e1c78f
HelenGonzalez <hgonzalez1@kippnyccp.org>
2015-06-01 13:24:39 -0400

    Update ultasonic_sensor_planning.py

--- ultasonic_sensor_planning.py
+++ ultasonic_sensor_planning.py
@@ -1,13 +1,31 @@
 from gopigo import *
+import time
+
 def turn_right():
   enc_tgt(1,0,14)
   time.sleep(.1)
   right()
   time.sleep(2)
   
-while (1 > 0)
+def turn_left():
+  enc_tgt(0,1,14)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
+  
+servo(90)
+
+while (1 > 0):
   if us_dist(15) > 10;
     fwd()
   else:
-    turn_right()
-  
+    stop()
+    servo(0)
+    left = us_dist(15)
+    servo(180)
+    right = us_dist(15)
+    if right > left:
+      turn_right()
+    else:
+      turn_left()
+    servo(90)
